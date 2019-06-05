b17d56be725ca94a544beea6b333d82a95e83411
andreas <andreas.arnolfo@gmail.com>
2019-06-05 10:20:35 +0200

    Crud

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,22 @@
 Rails.application.routes.draw do
   
+  get 'dogs/index'
+  get 'dogs/show'
+  get 'dogs/new'
+  get 'dogs/edit'
   resources :users
+  resources :dogs
   resources :sessions, only: [:new, :create, :destroy]
 
-  root to: 'pages#home'
+  root to: 'dogs#index'
 
   get 'signup', to: 'users#new', as: 'signup'
   get 'login', to: 'sessions#new', as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
+
+  get 'dogs/index'
+  get 'dogs/show'
+  get 'dogs/new'
+  get 'dogs/edit'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
